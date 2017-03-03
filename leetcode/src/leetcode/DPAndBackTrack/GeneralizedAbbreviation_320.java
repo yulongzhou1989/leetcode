066678164bb150f2bf3e1b2cb22fbaf4db702e52
yulongzhou1989 <yulongzhou1989@gmail.com>
2017-03-03 17:19:21 -0600

    x

---
+++ leetcode/src/leetcode/DPAndBackTrack/GeneralizedAbbreviation_320.java
@@ -0,0 +1,28 @@
+package leetcode.DPAndBackTrack;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GeneralizedAbbreviation_320 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(generateAbbreviations("word"));
+	}
+	
+	public static List<String> generateAbbreviations(String word) {
+        List<String> ret = new ArrayList<String> ();
+        backtrack(ret, word, 0,"",0);
+        return ret;
+    }
+	
+	private static void backtrack(List<String> ret, String word, int pos, String cur, int count){
+		if (pos == word.length()){
+			if (count>0) cur += count;
+			ret.add(cur);
+		} else{
+			backtrack(ret, word, pos+1, cur, count+1);
+			backtrack(ret, word, pos+1, cur+(count>0?count:"")+word.charAt(pos),0);
+		}
+	}
+}
