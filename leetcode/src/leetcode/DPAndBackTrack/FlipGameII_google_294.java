c830b6ef248659d90a697a6825054f919f57db0e
yulongzhou1989 <yulongzhou1989@gmail.com>
2017-03-07 09:34:03 -0600

    294 string&backtrack

--- leetcode/src/leetcode/DPAndBackTrack/FlipGameII_google_294.java
+++ leetcode/src/leetcode/DPAndBackTrack/FlipGameII_google_294.java
@@ -1,4 +1,4 @@
-package leetcode.StringArray;
+package leetcode.DPAndBackTrack;
 
 public class FlipGameII_google_294 {
 
@@ -8,11 +8,12 @@ public class FlipGameII_google_294 {
 	}
 	
 	public static boolean canWin(String s){
+		if (s.length()<0) return false;
         if (!s.contains("++")) return false;
         for (int i=0;i<s.length()-1;i++){
             if (s.charAt(i) == '+' && s.charAt(i+1) == '+'){
             	String temp = s.substring(0,i) + "--" + s.substring(i+2);
-                if (!temp.contains("++"))
+                if (!canWin(temp))
                 	return true;
             }
         }
