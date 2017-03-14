4be31127a20233a1b7731c6c35a5b958213ef2af
yulongzhou1989 <yulongzhou1989@gmail.com>
2017-03-14 16:58:02 -0500

    408 string

--- leetcode/src/leetcode/StringArray/ValidWordAbbreviation_google_408.java
+++ leetcode/src/leetcode/StringArray/ValidWordAbbreviation_google_408.java
@@ -4,17 +4,29 @@ public class ValidWordAbbreviation_google_408 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println(validWordAbbreviation("abbreviation",
+				"a10n"));
 	}
 
-	public boolean validWordAbbreviation(String word, String abbr) {
-        for(int i=0;i<abbr.length;i++){
-            if (word.length() == 0) return false;
-            //if is number, remove number length
-            if (abbr.charAt(i)>'0' && abbr.charAt(i)<='9'){
-                
-            }
-            //else word = word.sub(1);
-        }
-    }
+    public static boolean validWordAbbreviation(String word, String abbr) {
+        for(int i=0;i<abbr.length();){
+           if (word.length() == 0) return false;
+           //if is number, remove number length
+           String num = "";
+           if (abbr.charAt(i)>'0' && abbr.charAt(i)<='9' ){
+               while(i<abbr.length() && abbr.charAt(i)>='0' && abbr.charAt(i)<='9'){
+                   num += abbr.charAt(i++);
+               }
+               if (Integer.valueOf(num)>word.length()) return false;
+               word = word.substring(Integer.valueOf(num));
+           }
+           else {
+           	if (word.charAt(0)!=abbr.charAt(i++)) return false;
+               word = word.substring(1);
+           }
+       }
+       
+       if (word.length() == 0) return true;
+       else return false;
+   }
 }
