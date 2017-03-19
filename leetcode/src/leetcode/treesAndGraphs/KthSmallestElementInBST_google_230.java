59a7f5a6ed082b416ab5be2cc79047a6ff0f13f3
yulongzhou1989 <yulongzhou1989@gmail.com>
2017-03-18 22:52:43 -0500

    230 binarytree dfs

---
+++ leetcode/src/leetcode/treesAndGraphs/KthSmallestElementInBST_google_230.java
@@ -0,0 +1,31 @@
+package leetcode.treesAndGraphs;
+
+import common.TreeNode;
+
+public class KthSmallestElementInBST_google_230 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	int count;
+    int number = 0;
+    
+    public int kthSmallest(TreeNode root, int k) {
+        count = k;
+        inOrder(root);
+        return number;
+    }
+    
+    //inorder search
+    void inOrder(TreeNode root){
+        if (root.left != null) inOrder(root.left);
+        count--;
+        if (root!=null && count == 0) {
+            number = root.val;
+        }
+        if (root.right != null) inOrder(root.right);
+    }
+
+}
