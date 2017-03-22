f97b5f06a905ee9ad8b20e804128bc8908a3c3cd
yulongzhou1989 <yulongzhou1989@gmail.com>
2017-03-22 10:31:33 -0500

    341 design stack/queue 501 bst inorder

---
+++ leetcode/src/leetcode/design/FlattenNestedListIterator_google_341.java
@@ -0,0 +1,10 @@
+package leetcode.design;
+
+public class FlattenNestedListIterator_google_341 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ leetcode/src/leetcode/design/objects/NestedInterator.java
@@ -0,0 +1,60 @@
+package leetcode.design.objects;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * public interface NestedInteger {
+ *
+ *     // @return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     public boolean isInteger();
+ *
+ *     // @return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // Return null if this NestedInteger holds a nested list
+ *     public Integer getInteger();
+ *
+ *     // @return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // Return null if this NestedInteger holds a single integer
+ *     public List<NestedInteger> getList();
+ * }
+ */
+public class NestedIterator implements Iterator<Integer> {
+    
+    Queue<Integer> q;
+
+    public NestedIterator(List<NestedInteger> nestedList) {
+        q = new LinkedList<Integer>();
+        initialQ(nestedList);
+    }
+    
+    //flatten nestedList
+    private void initialQ(List<NestedInteger> nList){
+        for(int i=0;i<nList.size();i++){
+            if (nList.get(i).isInteger()){
+                q.offer(nList.get(i).getInteger());
+            }else {
+                initialQ(nList.get(i).getList());
+            }
+        }
+    }
+
+    @Override
+    public Integer next() {
+        return q.poll();
+    }
+
+    @Override
+    public boolean hasNext() {
+        return q.size()>0;
+    }
+}
+
+/**
+ * Your NestedIterator object will be instantiated and called as such:
+ * NestedIterator i = new NestedIterator(nestedList);
+ * while (i.hasNext()) v[f()] = i.next();
+ */
