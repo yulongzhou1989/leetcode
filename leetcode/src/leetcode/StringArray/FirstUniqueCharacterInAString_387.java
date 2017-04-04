5ed92b085d903f8eeacad8d98abddf689dda1a94
yulongzhou1989 <yulongzhou1989@gmail.com>
2017-04-04 00:43:56 -0500

    387 array

---
+++ leetcode/src/leetcode/StringArray/FirstUniqueCharacterInAString_387.java
@@ -0,0 +1,16 @@
+package leetcode.StringArray;
+
+public class FirstUniqueCharacterInAString_387 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public int firstUniqChar(String s) {
+        int [] freq = new int [26]; 
+        for(int i=0;i<s.length();i++) freq[s.charAt(i)-'a']++;
+        for(int i=0;i<s.length();i++) if(freq[s.charAt(i)-'a'] == 1) return i;
+        return -1;
+    }
+}
