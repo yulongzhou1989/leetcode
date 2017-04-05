6c42bcf2b84c55ff16105b82468b3301b9c00e06
yulongzhou1989 <yulongzhou1989@gmail.com>
2017-04-04 23:39:57 -0500

    268 bit xor

---
+++ leetcode/src/leetcode/bitManipulation/MissingNumber_google_268.java
@@ -0,0 +1,22 @@
+package leetcode.bitManipulation;
+
+public class MissingNumber_google_268 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public int missingNumber(int[] nums) {
+        int res = 0;
+        int i=0;
+        //a^b^b =a 
+        for(i=0;i<nums.length;i++){
+            res^=i^nums[i];
+        }
+        
+        //perfect situation it should be zero
+        return res^i;
+    }
+
+}
