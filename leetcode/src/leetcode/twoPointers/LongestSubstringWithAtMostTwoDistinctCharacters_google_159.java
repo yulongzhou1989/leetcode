d5f1f31503280e8fbedfef3f090538fadccbebd0
yulongzhou1989 <yulongzhou1989@gmail.com>
2017-04-25 16:56:42 -0500

    not finished:

---
+++ leetcode/src/leetcode/twoPointers/LongestSubstringWithAtMostTwoDistinctCharacters_google_159.java
@@ -0,0 +1,23 @@
+package leetcode.twoPointers;
+
+public class LongestSubstringWithAtMostTwoDistinctCharacters_google_159 {
+
+//	public int lengthOfLongestSubstringTwoDistinct(String s) {
+//        if(s.length()==0) return 0;
+//		int k = 2;
+//		int [] count = new int [256];
+//		HashSet<Character> set = new HashSet<>();
+//		int len = 0;
+//		
+//		for(int i=0,j=0;i<s.length();i++){
+//			set.add(s.charAt(i));
+//			len++;
+//			count[s.charAt(i)]++;
+//			while(set.size()>k){
+//				--;
+//			}
+//		}
+//	
+//		return len;
+//    }
+}
