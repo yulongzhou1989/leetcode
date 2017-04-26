358db6f8efcc1ea16da62cfcbb526fb0cd51ffa8
yulongzhou1989 <yulongzhou1989@gmail.com>
2017-04-26 09:31:22 -0500

    159 two pointers, slide window

--- leetcode/src/leetcode/twoPointers/LongestSubstringWithAtMostTwoDistinctCharacters_google_159.java
+++ leetcode/src/leetcode/twoPointers/LongestSubstringWithAtMostTwoDistinctCharacters_google_159.java
@@ -2,22 +2,26 @@ package leetcode.twoPointers;
 
 public class LongestSubstringWithAtMostTwoDistinctCharacters_google_159 {
 
-//	public int lengthOfLongestSubstringTwoDistinct(String s) {
-//        if(s.length()==0) return 0;
-//		int k = 2;
-//		int [] count = new int [256];
-//		HashSet<Character> set = new HashSet<>();
-//		int len = 0;
-//		
-//		for(int i=0,j=0;i<s.length();i++){
-//			set.add(s.charAt(i));
-//			len++;
-//			count[s.charAt(i)]++;
-//			while(set.size()>k){
-//				--;
-//			}
-//		}
-//	
-//		return len;
-//    }
+	public int lengthOfLongestSubstringTwoDistinct(String s) {
+        if(s.length()==0) return 0;
+		int k = 2;
+		int [] count = new int [256];
+		int len = 0;
+		int max = 0;
+		//slide window, j is the start point and i is the end point.
+		//if meet different character, len ++, if len>k, move left pointer,
+		//keeping pop out the character until pop out a different character to make the length
+		//back to k again.
+		for(int i=0,j=0;i<s.length();i++){
+			if(count[s.charAt(i)]++==0) len++;
+			if(len>k){
+				while(--count[s.charAt(j++)]>0);
+				len--;
+			}
+			
+			max = Math.max(max, i-j+1);
+		}
+	
+		return max;
+    }
 }
